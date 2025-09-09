5097de32f941cde9ab8e3115bf31bb43176f426e
Reza <rereitureza@gmail.com>
2025-09-09 12:46:40 +0000

    7

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,11 +6,6 @@
     <title>Dunia dalam Kekuatan</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        body { background-color: #f8f9fa; }
-        .navbar { margin-bottom: 2rem; }
-        .hide { display: none; }
-    </style>
     <?php if (isset($_GET['dev']) && $_GET['dev'] === '1'): ?>
     <style>
         .hide { display: block !important; }
