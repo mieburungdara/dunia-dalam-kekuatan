5530220f568749e11ddf1a0ef59b91fec7c17ac0
Reza <rereitureza@gmail.com>
2025-09-09 07:44:02 +0000

    ffffff

--- application/views/novel/arc_list_view.php
+++ application/views/novel/arc_list_view.php
@@ -1,11 +1,11 @@
 <div class="container">
-    <h1 class="mt-4 mb-3">Novel: <?php echo htmlspecialchars($this->get('novel_title')); ?></h1>
+    <h1 class="mt-4 mb-3">Novel: {{ @novel_title }}</h1>
 
     <div class="list-group">
-        <?php foreach ($this->get('arcs') as $arc): ?>
-            <a href="<?php echo $arc['url']; ?>" class="list-group-item list-group-item-action">
-                <?php echo htmlspecialchars($arc['name']); ?>
+        <repeat group="{{ @arcs }}" value="{{ @arc }}">
+            <a href="{{ @arc.url }}" class="list-group-item list-group-item-action">
+                {{ @arc.name }}
             </a>
-        <?php endforeach; ?>
+        </repeat>
     </div>
 </div>
