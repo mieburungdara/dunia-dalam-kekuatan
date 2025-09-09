217b540872efe5cf994c356e490ba08b477220e7
Reza <rereitureza@gmail.com>
2025-09-09 01:27:10 +0000

    dddd

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['baca/(:any)/(.+)'] = 'baca/index/$1/$2';
+
+$route['novel'] = 'novel/index';
+$route['novel/(:any)'] = 'novel/view/$1';
+$route['novel/(:any)/(:any)'] = 'novel/arc/$1/$2';
+$route['novel/(:any)/(:any)/(:any)'] = 'novel/chapter/$1/$2/$3';
+$route['novel/(:any)/(:any)/(:any)/(:any)'] = 'novel/scene/$1/$2/$3/$4';
+
