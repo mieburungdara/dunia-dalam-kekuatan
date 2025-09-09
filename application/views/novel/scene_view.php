c54c3ae97b3f8d0a8c60db0c6d8345cbf8839989
Reza <rereitureza@gmail.com>
2025-09-09 08:52:10 +0000

    ggggggggg

--- application/views/novel/scene_view.php
+++ application/views/novel/scene_view.php
@@ -2,7 +2,7 @@
     <h4 class="mb-4"><small class="text-muted">Novel: {{ @novel_title }}</small></h4>
     <h3 class="mb-3"><small class="text-muted">Arc: {{ @arc_title }}</small></h3>
     <h2 class="mb-3"><small class="text-muted">Chapter: {{ @chapter_title }}</small></h2>
-    <p class="mb-3"><small class="text-muted">Summary: {{ @chapter_summary }}</small></p>
+    <h2 class="mb-3"><small class="text-muted">{{ @chapter_summary }}</small></h2>
     <h1 class="mt-4 mb-3">Scene: {{ @scene_name }}</h1>
 
 
