328d0c4bb0bf470a859a051eebf4960e08c8052d
Reza <rereitureza@gmail.com>
2025-09-09 08:50:38 +0000

    aaaaaaaaaaaaa

--- application/views/novel/scene_view.php
+++ application/views/novel/scene_view.php
@@ -1,10 +1,10 @@
 <div class="container">
-    <h1 class="mt-4 mb-3">Scene: {{ @scene_name }}</h1>
-
+    <h4 class="mb-4"><small class="text-muted">Novel: {{ @novel_title }}</small></h4>
+    <h3 class="mb-3"><small class="text-muted">Arc: {{ @arc_title }}</small></h3>
     <h2 class="mb-3"><small class="text-muted">Chapter: {{ @chapter_title }}</small></h2>
     <p class="mb-3"><small class="text-muted">Summary: {{ @chapter_summary }}</small></p>
-    <h3 class="mb-3"><small class="text-muted">Arc: {{ @arc_title }}</small></h3>
-    <h4 class="mb-4"><small class="text-muted">Novel: {{ @novel_title }}</small></h4>
+    <h1 class="mt-4 mb-3">Scene: {{ @scene_name }}</h1>
+
 
     <div class="scene-content">
         {{ @scene_content | raw }}
