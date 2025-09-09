358b9df19767c34a64e5d45351bd61a04b52865f
Reza <rereitureza@gmail.com>
2025-09-09 12:47:39 +0000

    8

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <?php if (isset($_GET['dev']) && $_GET['dev'] === '1'): ?>
     <style>
-        .hide { display: block !important; }
+        .hide { display: none !important; }
         .dev-info { border: 1px dashed #ccc; padding: 5px; margin-top: 5px; background-color: #f0f0f0; }
     </style>
     <?php endif; ?>
