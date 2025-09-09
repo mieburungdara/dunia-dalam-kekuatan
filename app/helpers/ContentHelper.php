f0f5474beeec8fe9a74948250c788acb6b0909e8
Reza <rereitureza@gmail.com>
2025-09-09 13:45:03 +0000

    22

--- app/helpers/ContentHelper.php
+++ app/helpers/ContentHelper.php
@@ -24,7 +24,7 @@ class ContentHelper
 
                 case 'InnerThought':
                     $charName = isset($item['Character']['Name']) ? htmlspecialchars($item['Character']['Name'], ENT_QUOTES, 'UTF-8') : 'Seseorang';
-                    $html .= '<div class="inner-thought-block bg-light border-start border-info border-3 ps-3 py-2 my-3">';
+                    $html .= '<div class="inner-thought-block bg-body-secondary border-start border-info border-3 ps-3 py-2 my-3">';
                     $html .= "  <small class=\"text-muted\">Pikiran {$charName}:</small>";
                     $html .= "  <p class=\"mb-0 fst-italic\">\"{$text}\"</p>";
                     $html .= '</div>';
@@ -58,7 +58,7 @@ class ContentHelper
                 case 'Decision':
                 case 'Strategy':
                     $charName = isset($item['Character']['Name']) ? htmlspecialchars($item['Character']['Name'], ENT_QUOTES, 'UTF-8') : 'Seseorang';
-                    $html .= '<div class="decision-block bg-light border-start border-success border-3 ps-3 py-2 my-3">';
+                    $html .= '<div class="decision-block bg-body-secondary border-start border-success border-3 ps-3 py-2 my-3">';
                     $html .= "  <small class=\"text-muted\">Keputusan / Strategi oleh {$charName}:</small>";
                     $html .= "  <p class=\"mb-0\">{$text}</p>";
                     $html .= '</div>';
