35444787b892c085e54ff97cb139e3de05158bc2
Reza <rereitureza@gmail.com>
2025-09-09 12:51:20 +0000

    11

--- app/helpers/ContentHelper.php
+++ app/helpers/ContentHelper.php
@@ -37,10 +37,11 @@ class ContentHelper
 
                 case 'InnerThought':
                     $charName = isset($item['Character']['Name']) ? htmlspecialchars($item['Character']['Name'], ENT_QUOTES, 'UTF-8') : 'Seseorang';
-                    $html .= "<p class=\"inner-thought\"><i>\"";
-                    $html .= "{$text}";
-                    $html .= "\"</i></p>";
-                    $html .= "<div class=\"hide small text-muted border-top mt-1 pt-1\"><em>Type: InnerThought | Character: {$charName}</em></div>";
+                    $html .= '<div class="inner-thought-block bg-light border-start border-info border-3 ps-3 py-2 my-3">';
+                    $html .= "  <small class=\"text-muted\">Pikiran {$charName}:</small>";
+                    $html .= "  <p class=\"mb-0 fst-italic\">\"{$text}\"</p>";
+                    $html .= "  <div class=\"hide small text-muted border-top mt-1 pt-1\"><em>Type: InnerThought | Character: {$charName}</em></div>";
+                    $html .= '</div>';
                     break;
 
                 case 'Emotion':
