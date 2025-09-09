aacac23c4becbdb79c6db7a216d636e29214f126
Reza <rereitureza@gmail.com>
2025-09-09 07:57:50 +0000

    dddddd

--- tmp/3lwv2jbrdveoc.vfqu5ixhrw2l.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="px-4 py-5 mb-4 text-center">
-        <h1 class="display-5 fw-bold">Dunia dalam Kekuatan</h1>
-        <div class="col-lg-8 mx-auto">
-            <p class="lead mb-4">Selamat datang di dunia fantasi yang penuh dengan keajaiban dan petualangan. Klik tombol di bawah untuk melihat daftar novel.</p>
-            <a href="/novel" class="btn btn-primary btn-lg">Lihat Daftar Novel</a>
-        </div>
-    </div>
-</div>
