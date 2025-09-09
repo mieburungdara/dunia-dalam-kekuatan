d225dcf2591b28a9e790f61a8e5ccd4cad7d4a04
Reza <rereitureza@gmail.com>
2025-09-09 07:43:37 +0000

    aa

--- application/views/novel/arc_list_view.php
+++ application/views/novel/arc_list_view.php
@@ -1,22 +1,8 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
 <div class="container">
-
-<h1>{{ @novel_title }}</h1>
-<p>Slug: {{ @novel_slug }}</p>
-
-<ul>
-    <repeat group="{{ @arcs }}" value="{{ @arc }}">
-        <li>{{ @arc.name }}</li>
-    </repeat>
-</ul>
-
-
-    <h1 class="mt-4 mb-3">Novel: <?php echo htmlspecialchars($novel_title); ?></h1>
+    <h1 class="mt-4 mb-3">Novel: <?php echo htmlspecialchars($this->get('novel_title')); ?></h1>
 
     <div class="list-group">
-        <?php foreach ($arcs as $arc): ?>
+        <?php foreach ($this->get('arcs') as $arc): ?>
             <a href="<?php echo $arc['url']; ?>" class="list-group-item list-group-item-action">
                 <?php echo htmlspecialchars($arc['name']); ?>
             </a>
