51751d8160ce8bcc4bcf5b1a2c6c380272199ce8
Reza <rereitureza@gmail.com>
2025-09-09 07:57:04 +0000

    aa

--- application/views/novel/arc_list_view.php
+++ application/views/novel/arc_list_view.php
@@ -1,5 +1,6 @@
 <div class="container">
     <h1 class="mt-4 mb-3">Novel: {{ @novel_title }}</h1>
+    <h2 class="mb-3"><small class="text-muted">Slug: {{ @novel_slug }}</small></h2>
 
     <div class="list-group">
         <repeat group="{{ @arcs }}" value="{{ @arc }}">
