a8521cc6889e4be581d3095850dd3a9a7d4e9c24
Reza <rereitureza@gmail.com>
2025-09-09 07:38:37 +0000

    aaaaaa

--- application/views/novel/novel_list_view.php
+++ application/views/novel/novel_list_view.php
@@ -2,11 +2,12 @@
     <h1 class="mt-4 mb-3">Daftar Novel</h1>
 
     <div class="list-group">
-        <?php foreach ($this->get('novels') as $novel): ?>
-    <a href="<?php echo $novel['url']; ?>" class="list-group-item list-group-item-action">
-        <?php echo $novel['title']; ?>
+        <?php var_dump($f3->get('novels'));?>
+        <repeat group="{{ @novels }}" value="{{ @novel }}">
+    <a href="{{ @novel.url }}" class="list-group-item list-group-item-action">
+        {{ @novel.title }}
     </a>
-<?php endforeach; ?>
+</repeat>
 
     </div>
 </div>
