c24c5b736c8c124ace760d65f9c94556e99b97a3
Reza <rereitureza@gmail.com>
2025-09-09 08:03:25 +0000

    zzzzzzzz

--- application/views/novel/chapter_list_view.php
+++ application/views/novel/chapter_list_view.php
@@ -1,7 +1,5 @@
 <div class="container">
     <h1 class="mt-4 mb-3">Arc: {{ @arc_title }}</h1>
-    <h2 class="mb-3"><small class="text-muted">Arc Name: {{ @arc_name }}</small></h2>
-    <h3 class="mb-4"><small class="text-muted">Novel Slug: {{ @novel_slug }}</small></h3>
 
     <div class="list-group">
         <repeat group="{{ @chapters }}" value="{{ @chapter }}">
