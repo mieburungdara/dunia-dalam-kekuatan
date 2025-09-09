b4eb7cec85269e48e75f78fa2885669d0af053e6
Reza <rereitureza@gmail.com>
2025-09-09 07:37:20 +0000

    aaaa

--- application/views/novel/novel_list_view.php
+++ application/views/novel/novel_list_view.php
@@ -2,11 +2,11 @@
     <h1 class="mt-4 mb-3">Daftar Novel</h1>
 
     <div class="list-group">
-        <?php var_dump($f3->get('novels'));?>
-        <?php foreach ($novels as $novel): ?>
-            <a href="<?php echo $novel['url']; ?>" class="list-group-item list-group-item-action">
-                <?php echo $novel['title']; ?>
-            </a>
-        <?php endforeach; ?>
+        <?php foreach ($this->get('novels') as $novel): ?>
+    <a href="<?php echo $novel['url']; ?>" class="list-group-item list-group-item-action">
+        <?php echo $novel['title']; ?>
+    </a>
+<?php endforeach; ?>
+
     </div>
 </div>
